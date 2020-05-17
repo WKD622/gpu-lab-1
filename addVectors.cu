9e3ecbaa4b543bb1e5b9253be96c7f5f0b94e078
WKD622 <kuba.ziarko@gmail.com>
2020-05-17 17:46:43 +0200

    c

--- addVectors.cu
+++ addVectors.cu
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <cuda.h>
 #include <stdlib.h>
-#define N 10
+#include <helper_timer.h>
+#include <time.h>
 
 __global__ void add(int *a, int *b, int *c)
 {
@@ -11,18 +12,41 @@ __global__ void add(int *a, int *b, int *c)
         c[tid] = a[tid] + b[tid];
     }
 }
+
+void cpu_add(int *a, int *b, int *c, int N)
+{
+    int i;
+    for (i = 0; i < N; i++)
+    {
+        c[i] = a[i] + b[i];
+    }
+}
+
+bool check_ans(int* c, int* dev_c, int N) {
+    int i;
+    bool are_right = true;
+ 
+    for (i = 0; i < N; i++) {
+        are_right = are_right && (c[i] == dev_c[i]);
+    }
+ 
+    return are_right;
+}
+
 int main(void)
 {
-    int a[N], b[N], c[N];
+
+    int N = atoi(argv[1]);
+    int block_size = atoi(argv[2]);
+    int grid_size = (int)ceil((float)N / block_size);
+
+    int *a, *b, *c, *c_cpu;
+    a = (int *)malloc(N * sizeof(int));
+    b = (int *)malloc(N * sizeof(int));
+    c = (int *)malloc(N * sizeof(int));
+    c_cpu = (int *)malloc(N * sizeof(int));
+
     int *dev_a, *dev_b, *dev_c;
-
-    cudaMalloc((void **)&a_d, size); // alokuj pamięć na GPU
-    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
-
-    StopWatchInterface *timer = NULL;
-    sdkCreateTimer(&timer);
-    sdkResetTimer(&timer);
-    sdkStartTimer(&timer);
     cudaMalloc((void **)&dev_a, N * sizeof(int));
     cudaMalloc((void **)&dev_b, N * sizeof(int));
     cudaMalloc((void **)&dev_c, N * sizeof(int));
@@ -31,29 +55,34 @@ int main(void)
         a[i] = i;
         b[i] = i * 2;
     }
-    cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_c, c, N * sizeof(int), cudaMemcpyHostToDevice);
-    add<<<1, N>>>(dev_a, dev_b, dev_c);
-    cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
 
-    kernel<<<n_blocks, block_size>>>(a_d, N);
+    StopWatchInterface *timer = NULL;
+    sdkCreateTimer(&timer);
+    sdkResetTimer(&timer);
+    sdkStartTimer(&timer);
+
+    add<<<grid_size, block_size>>>(dev_a, dev_b, dev_c, N);
+
     cudaThreadSynchronize();
     sdkStopTimer(&timer);
-    float time = sdkGetTimerValue(&timer);
+    float gpu_time = sdkGetTimerValue(&timer);
     sdkDeleteTimer(&timer);
+    cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
 
-    printf("dupa1", a[i], b[i], c[i]);
-    for (int i = 0; i < N; i++)
+    clock_t t1, t2;
+    t1 = clock();
+    cpu_add(a, b, c_cpu, N);
+    t2 = clock();
+    float cpu_time = ((float)(t2 - t1) / CLOCKS_PER_SEC) * 1000;
+
+    if (check_ans(c_cpu, c, N))
     {
-        printf("%d+%d=%d\n", a[i], b[i], c[i]);
+        printf("%f,%f,%d,%d\n", gpu_time, cpu_time, block_size, grid_size);
     }
-    
-    printf("dupa2", a[i], b[i], c[i]);
-
-    cudaMemcpy(a_h, a_d, sizeof(float) * N, cudaMemcpyDeviceToHost);
-    cudaFree(a_d);
-    printf("Time for the kernel: %f ms\n", time);
+    else
+    {
+        printf("WRONG CALCULATIONS\n");
+    }
 
     cudaFree(dev_a);
     cudaFree(dev_b);
